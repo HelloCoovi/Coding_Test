d730fc2edcd7336169e2e695030b40716079b7cb
jangg0627 <85853962+jangg0627@users.noreply.github.com>
2022-10-26 16:13:07 +0900

    [Bronze III] Title: 최댓값, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -0,0 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+
+nums = [list(map(int, input().split())) for _ in range(9)]
+nums = sum(nums, [])
+
+
+print(max(nums))
+
+print(f"{nums.index(max(nums)) // 9 + 1} {nums.index(max(nums)) % 9 + 1}")
