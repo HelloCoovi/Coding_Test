4e8a7a1d156e5a2942a041b2998a1a630b2fcd5d
jangg0627 <85853962+jangg0627@users.noreply.github.com>
2022-11-06 00:22:25 +0900

    [Silver V] Title: 소수, Time: 88 ms, Memory: 32952 KB -BaekjoonHub

--- 백준/Silver/2581. 소수/소수.py
+++ 백준/Silver/2581. 소수/소수.py
@@ -1,19 +1,31 @@
-start_num = int(input())
-last_num = int(input())
 
-sosu_list = []
-for num in range(start_num, last_num+1):
-    error = 0
-    if num > 1 :
-        for i in range(2, num):  # 2부터 num-1까지
+import math
+
+def is_priem_num(num):
+    if num == 1:
+        return False
+    else:
+        for i in range(2, int(math.sqrt(num)) + 1):
             if num % i == 0:
-                error += 1
-                break  # 2부터 num-1까지 나눈 몫이 0이면 error가 증가하고 for문을 끝냄
-        if error == 0:
-            sosu_list.append(num)  # error가 없으면 소수리스트에 추가
-            
-if len(sosu_list) > 0 :
-    print(sum(sosu_list))
-    print(min(sosu_list))
-else:
+                return False
+    return True
+    
+
+start = int(input())
+end = int(input())
+
+reslut = 0
+min_num = 0
+
+for num in range(start, end + 1):
+    if is_priem_num(num) == True and min_num == 0:
+        reslut += num
+        min_num = num
+    elif is_priem_num(num) == True:
+        reslut += num
+
+if reslut == 0:
     print(-1)
+else:
+    print(reslut)
+    print(min_num)
