be95eae50f1332b4a4956e39afe5d25ce75c7c28
jangg0627 <85853962+jangg0627@users.noreply.github.com>
2022-11-08 00:00:49 +0900

    [Silver III] Title: 베르트랑 공준, Time: 1300 ms, Memory: 32952 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,37 +1,26 @@
-# 테스트 케이스 총 범위
-n=123456
+import math
+import sys
+input = sys.stdin.readline
 
-# 에라토스테네스의 체
-def prime_list(n):
-    if n == 1:
-        return False
-    for i in range(2,int(n**0.5)+1):
-        if n%i == 0:
+def is_prime_num(num):
+    for n in range(2, int(math.sqrt(num)) + 1):
+        if num % n == 0:
             return False
-    return True		
+    return True
 
-primes_list = list(range(2,246912))
-ans_list = []
-
-for i in primes_list:
-    # 만약 소수에 해당하면 prime_list 함수에서 true를 반환 받을 것입니다.
-    if prime_list(i):
-        # true를 반환 받는다면 ans_list에 해당 원소(i)를 추가해줍니다.
-        ans_list.append(i)
-        
-        
-while True:
+prime_nums = [num for num in range(2, 246913) if is_prime_num(num)]
     
-    cnt = 0
-
+while True:
+    count = 0
     num = int(input())
-
-    # 0을 입력받으면 종료    
     if num == 0:
         break
-    
-    for i in ans_list:
-        if num < i < 2*num+1:
-            cnt += 1
+        
+    # for i in range(num + 1, num * 2 + 1):
+    #     if i in prime_nums:
+    #         count += 1
+    for i in prime_nums:
+        if num < i <= num * 2:
+            count += 1
             
-    print(cnt)
+    print(count)
