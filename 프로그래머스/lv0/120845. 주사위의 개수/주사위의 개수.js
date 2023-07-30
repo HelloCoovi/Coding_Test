f89c892627b4607aaf7d48534b14276b192b3cb9
Coovi <85853962+jangg0627@users.noreply.github.com>
2023-07-31 04:02:11 +0900

    [level 0] Title: 주사위의 개수, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,9 +1,4 @@
 function solution(box, n) {
-    let answer = 1
-    
-    box.map((len) => {
-        answer *= Math.floor(len / n)
-    })
-    
-    return answer
+    var answer = 0;
+    return answer;
 }
