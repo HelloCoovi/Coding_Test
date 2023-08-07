88d414392eaf27e46f7f916df3081d37246e762d
Coovi <85853962+jangg0627@users.noreply.github.com>
2023-08-07 16:13:56 +0900

    [level 0] Title: 소인수분해, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    let answer = []
+    let origNum = n
+    
+    let i = 2
+    while (i <= n) {
+        if (n % i === 0) {
+            answer = [...answer, i];
+            n = n / i;
+        } else {
+            i++;
+        }
+    }
+    
+    return [...new Set(answer)]
+}
