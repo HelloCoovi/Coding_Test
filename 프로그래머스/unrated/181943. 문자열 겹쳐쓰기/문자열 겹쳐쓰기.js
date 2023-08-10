070142ab201d65f226ed8f2526ac87991052aca4
Coovi <85853962+jangg0627@users.noreply.github.com>
2023-08-11 01:56:38 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -0,0 +1,12 @@
+function solution(my_string, overwrite_string, s) {    
+    return my_string.slice(0, s) + overwrite_string + my_string.slice(s + overwrite_string.length)
+}
+
+// s == 2 -> index
+// He11oWor1d
+//   lloWorl
+// HelloWorld
+
+// He
+//   lloWorl
+//          d
