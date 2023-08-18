f7091e6a86b28ea20c78ca14652397a56f4171fa
Coovi <85853962+jangg0627@users.noreply.github.com>
2023-08-19 01:23:56 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,16 @@
+function solution(numbers) {
+    let result = "";
+    const numObj = { "zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five":5, "six": 6, "seven": 7, "eight": 8, "nine": 9 };
+    
+    let word = "";
+    for (let i = 0; i < numbers.length; i++ ) {
+        word += numbers[i];
+        
+        if (Object.keys(numObj).includes(word)) {
+            result += numObj[word];
+            word = "";
+        }
+    }
+    
+    return +result;
+}
