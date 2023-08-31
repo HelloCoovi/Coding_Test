1c5013dc1ac368bc3629c0dc58f6506e7bae0df2
Coovi <85853962+jangg0627@users.noreply.github.com>
2023-08-31 14:11:05 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
@@ -1,3 +1,4 @@
 function solution(my_string) {
-    return my_string.split("").map(char => char.toLowerCase()).sort().join("")
+    // return my_string.split("").map(char => char.toLowerCase()).sort().join("")
+    return [...my_string.toLowerCase()].sort().join("")
 }
