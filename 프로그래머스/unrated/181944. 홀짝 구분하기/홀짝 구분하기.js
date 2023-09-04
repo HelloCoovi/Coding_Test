9ab548c018c20a2f946b00ab594ee6db6e234732
Coovi <85853962+jangg0627@users.noreply.github.com>
2023-09-04 22:39:27 +0900

    [unrated] Title: 홀짝 구분하기, Time: 46.78 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    n = Number(input[0]);
+    
+    console.log(n % 2 === 1 ? `${n} is odd` : `${n} is even`)
+});
