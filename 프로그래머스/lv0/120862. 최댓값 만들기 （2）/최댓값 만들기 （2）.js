0b9c8485e45e3b6a3b623fd88c507f9f1620406c
Coovi <85853962+jangg0627@users.noreply.github.com>
2023-09-05 16:06:59 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,11 +1,14 @@
 function solution(numbers) {
-    if (numbers.length === 2) return numbers[0] * numbers[1]
+//     if (numbers.length === 2) return numbers[0] * numbers[1]
     
-    const posNums = numbers.filter(num => num > 0).sort((a, b) => b - a)
-    const negNums = numbers.filter(num => num < 0).sort((a, b) => a - b)
+//     const posNums = numbers.filter(num => num > 0).sort((a, b) => b - a)
+//     const negNums = numbers.filter(num => num < 0).sort((a, b) => a - b)
     
-    const posMulti = !isNaN(posNums[0] * posNums[1]) ? posNums[0] * posNums[1] : 0
-    const negMulti = !isNaN(negNums[0] * negNums[1]) ? negNums[0] * negNums[1] : 0
+//     const posMulti = !isNaN(posNums[0] * posNums[1]) ? posNums[0] * posNums[1] : 0
+//     const negMulti = !isNaN(negNums[0] * negNums[1]) ? negNums[0] * negNums[1] : 0
     
-    return  posMulti > negMulti ? posMulti : negMulti
+//     return  Math.max(posMulti, negMulti)
+    const sortedNums = numbers.sort((a, b) => b - a)
+    
+    return Math.max(sortedNums[0] * sortedNums[1] , sortedNums[sortedNums.length - 1] * sortedNums[sortedNums.length - 2])
 }
