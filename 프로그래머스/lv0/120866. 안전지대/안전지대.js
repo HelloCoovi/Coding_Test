dc8983347dfc272f090283a0923abf434f1ff39c
Coovi <85853962+jangg0627@users.noreply.github.com>
2023-09-08 01:33:29 +0900

    [level 0] Title: 안전지대, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.js
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.js
@@ -1,23 +1,33 @@
 function solution(board) {
-    let pos = []
+//     let pos = []
     
-    board.forEach((_, xIdx) => {
-        if (board[xIdx].includes(1)) {
-            board[xIdx].forEach((num, yIdx) => {
-                if (num === 1) pos.push([xIdx, yIdx])
-            })
-        }
-    })
+//     board.forEach((_, xIdx) => {
+//         if (board[xIdx].includes(1)) {
+//             board[xIdx].forEach((num, yIdx) => {
+//                 if (num === 1) pos.push([xIdx, yIdx])
+//             })
+//         }
+//     })
 
-    pos.forEach(el => {
-        for (let i = -1; i < 2; i++) {
-            for (let j = -1; j < 2; j++) {
-                if (board[el[0] + i] && board[el[0] + i][el[1] + j] + 1) {
-                    board[el[0] + i][el[1] + j] = 1
-                }
-            }
-        }
-    })
+//     pos.forEach(el => {
+//         for (let i = -1; i < 2; i++) {
+//             for (let j = -1; j < 2; j++) {
+//                 if (board[el[0] + i] && board[el[0] + i][el[1] + j] + 1) {
+//                     board[el[0] + i][el[1] + j] = 1
+//                 }
+//             }
+//         }
+//     })
     
-    return board.flat().filter(num => num === 0).length
+//     return board.flat().filter(num => num === 0).length
+    
+    let outside = [[-1,0], [-1,-1], [-1,1], [0,-1],[0,1],[1,0], [1,-1], [1,1]];
+    let safezone = 0;
+
+    board.forEach((row, y, self) => row.forEach((el, x) => {
+        if (el === 1) return;
+        return outside.some(([oy, ox]) => self[oy + y]?.[ox + x]) ? "" : safezone++;
+    }));
+
+    return safezone;
 }
