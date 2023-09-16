8458d2c6d98d988301765e573f07546bf83c4e49
Coovi <85853962+jangg0627@users.noreply.github.com>
2023-09-17 02:37:49 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -3,7 +3,7 @@ function solution(n) {
 
     for (let i = 0; i < n; i++ ) {
         result++
-        while (result % 10 === 3 || result % 3 === 0 || String(result).includes("3")) result += 1
+        while (result % 3 === 0 || String(result).includes("3")) result += 1
     }
 
     return result
