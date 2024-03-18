cff2bc6e713cf2f737c6ad50a66a3e2a2c70b36a
Coovi <85853962+HelloCoovi@users.noreply.github.com>
2024-03-18 23:02:03 +0900

    [Bronze IV] Title: 세수정렬, Time: 124 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.js
+++ 백준/Bronze/2752. 세수정렬/세수정렬.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+const result = input[0].split(' ').map(Number)
 
-const result = input[0].split(" ").map(Number)
-result.sort((a, b) => a - b)
+result.sort((a, b) => a - b);
 
-console.log(result.join(" "))
+console.log(result.join(" "));
